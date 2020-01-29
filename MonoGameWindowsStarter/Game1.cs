1943da4397aa0fb387bb012f9e78fa98c31472fd
Tanner Driggers <tannerdriggers@ksu.edu>
2020-01-29 16:57:55 -0600

    init

--- MonoGameWindowsStarter/Game1.cs
+++ MonoGameWindowsStarter/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System;
+using System.Timers;
 
 namespace MonoGameWindowsStarter
 {
@@ -9,8 +12,35 @@ namespace MonoGameWindowsStarter
     /// </summary>
     public class Game1 : Game
     {
+        private class Enemy
+        {
+            private bool alive = true;
+            private Rectangle position = new Rectangle(2000, 0, 150, 150);
+            private int speed;
+
+            public Enemy(int speed, int YPosition)
+            {
+                this.speed = speed;
+                position.Y = YPosition;
+            }
+
+            public int Speed { get => speed; }
+            public bool Alive { get => alive; set => alive = value; }
+            public Rectangle Position { get => position; set => position = value; }
+        }
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Texture2D playerSprite;
+        Rectangle playerPosition;
+        Texture2D enemySprite;
+        List<Enemy> enemies;
+        int enemySpeed = 8;
+        SpriteFont scoreText;
+        Random random;
+        Timer timer;
+        int score = 0;
+        Vector2 scorePosition;
 
         public Game1()
         {
@@ -27,6 +57,18 @@ namespace MonoGameWindowsStarter
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.PreferredBackBufferHeight = 1080;
+            graphics.ApplyChanges();
+            random = new Random();
+
+            timer = new Timer(1000);
+            timer.Elapsed += AddEnemy;
+            timer.Start();
+
+            scorePosition = new Vector2(GraphicsDevice.Viewport.Width - 200, 50);
+
+            enemies = new List<Enemy>();
 
             base.Initialize();
         }
@@ -41,6 +83,10 @@ namespace MonoGameWindowsStarter
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            enemySprite = Content.Load<Texture2D>("piranha");
+            playerSprite = Content.Load<Texture2D>("fish");
+            scoreText = Content.Load<SpriteFont>("Score");
+            playerPosition = new Rectangle(50, GraphicsDevice.Viewport.Height / 2, playerSprite.Width / 11, playerSprite.Height / 11);
         }
 
         /// <summary>
@@ -50,6 +96,30 @@ namespace MonoGameWindowsStarter
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
+        }
+
+        private void AddEnemy(object source, ElapsedEventArgs e)
+        {
+            // chooses when to speed up the enemies
+            if (score % 2 == 0 && enemySpeed < 200)
+            {
+                enemySpeed++;
+
+                if (timer.Interval > 50)
+                {
+                    timer.Stop();
+                    timer.Interval -= 50;
+                    timer.Start();
+                }
+            }
+            enemies.Add(new Enemy(enemySpeed, random.Next(-20, 980)));
+        }
+
+        private void EndGame()
+        {
+            timer.Stop();
+            enemies = new List<Enemy>();
+            scorePosition = new Vector2(GraphicsDevice.Viewport.Width / 2 - 50, GraphicsDevice.Viewport.Height / 2 - 20);
         }
 
         /// <summary>
@@ -62,7 +132,48 @@ namespace MonoGameWindowsStarter
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                var enemy = enemies[i];
+                if (enemy.Alive)
+                {
+                    enemy.Position = new Rectangle(enemy.Position.X - enemy.Speed, enemy.Position.Y, enemy.Position.Width, enemy.Position.Height);
+
+                    if (enemy.Position.X < -enemySprite.Width)
+                    {
+                        enemy.Alive = false;
+                        score += 1;
+                    }
+                    else if (playerPosition.Intersects(enemy.Position))
+                    {
+                        EndGame();
+                    }
+                }
+                else
+                {
+                    enemies.RemoveAt(i);
+                }
+            }
+
+            // player position
+            var playerSpeed = 15;
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                playerPosition.Y -= playerSpeed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                playerPosition.Y += playerSpeed;
+            }
+
+            if (playerPosition.Y < 0)
+            {
+                playerPosition.Y = 0;
+            }
+            if (playerPosition.Y > GraphicsDevice.Viewport.Height - playerPosition.Height)
+            {
+                playerPosition.Y = GraphicsDevice.Viewport.Height - playerPosition.Height;
+            }
 
             base.Update(gameTime);
         }
@@ -76,6 +187,21 @@ namespace MonoGameWindowsStarter
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            for (var i = 0; i < enemies.Count; i++)
+            {
+                var enemy = enemies[i];
+                if (enemy.Alive)
+                {
+                    spriteBatch.Draw(enemySprite, new Rectangle(enemy.Position.X, enemy.Position.Y, 150, 150), Color.White);
+                }
+            }
+
+            spriteBatch.Draw(playerSprite, playerPosition, Color.White);
+
+            spriteBatch.DrawString(scoreText, score.ToString(), scorePosition, Color.Black);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
